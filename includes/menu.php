d4f1562d5105323682cdd3e6f5af1ef11dbd312b
Gopi <info@gopir13.com>
2015-06-08 17:42:47 +0530

    add php file

---
+++ includes/menu.php
@@ -0,0 +1,14 @@
+<?php 
+?>
+<!-------------Main Menu------------>
+ <div id="main-menu" class="row text-center">
+                <ul>
+                    <li><a class="active" href="">Home</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">Company</a></li>
+                    <li><a href="">Portfolio</a></li>
+                    <li><a href="">Clients</a></li>
+                    <li><a href="">Reach Us</a></li>
+                </ul>
+ </div>
+ <!------------End Menu------------->
